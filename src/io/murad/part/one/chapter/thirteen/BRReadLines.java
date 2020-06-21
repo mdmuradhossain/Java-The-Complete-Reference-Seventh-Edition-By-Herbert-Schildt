2207e3cbf21ac09dd872506dc8e771213d191cdf
= <=mdmuradhossain33@gmail.com>
2020-06-21 19:41:22 +0600

    reading strings example added

---
+++ src/io/murad/part/one/chapter/thirteen/BRReadLines.java
@@ -0,0 +1,24 @@
+package io.murad.part.one.chapter.thirteen;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//Read a string from console using a BufferedReader.
+public class BRReadLines {
+
+	public static void main(String[] args) throws IOException {
+		// create a BufferedReader using System.in
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str;
+		System.out.println("Enter lines of text.");
+		System.out.println("Enter 'stop' to quit.");
+
+		do {
+			str = br.readLine();
+			System.out.println(str);
+		} while (!str.equals("stop"));
+
+	}
+
+}
