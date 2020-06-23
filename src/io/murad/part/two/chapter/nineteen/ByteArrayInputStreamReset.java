83af159c7547e847276395428ab9c21de0019b1f
= <=>
2020-06-23 13:25:59 +0600

    ByteArrayInputStream reset ex added

---
+++ src/io/murad/part/two/chapter/nineteen/ByteArrayInputStreamReset.java
@@ -0,0 +1,28 @@
+package io.murad.part.two.chapter.nineteen;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+public class ByteArrayInputStreamReset {
+
+	public static void main(String[] args) throws IOException{
+		
+		String tmp = "abc";
+		byte b[] = tmp.getBytes();
+		ByteArrayInputStream in = new ByteArrayInputStream(b);
+		
+		for (int i = 0; i < 2; i++) {
+			int c;
+			while ((c=in.read()) != -1) {
+				if (i==0) {
+					System.out.println((char) c);
+				}else {
+					System.out.println(Character.toUpperCase((char) c));
+				}
+			}
+			System.out.println();
+			in.reset();
+		}
+	}
+
+}
